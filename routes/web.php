a5b14ea90548e381f2a1215f865009f2db6172e3
iezakelMcShane <dimon13rusmr@gmail.com>
2025-02-23 00:25:33 +0300

    Задание 3.8

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,22 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{year}/{month}/{day}', function ($year, $month, $day) {
-    // Проверяем корректность параметров
-    if (preg_match('/^\d{4}$/', $year) && preg_match('/^(0[1-9]|1[0-2])$/', $month) && preg_match('/^(0[1-9]|[12][0-9]|3[01])$/', $day)) {
-        // Проверка на валидную дату
-        $dateString = "{$year}-{$month}-{$day}";
-        $date = DateTime::createFromFormat('Y-m-d', $dateString);
+Route::get('/user/{name}', function ($name) {
+    $users = [ 
+        'user1' => 'city1', 
+        'user2' => 'city2', 
+        'user3' => 'city3', 
+        'user4' => 'city4', 
+        'user5' => 'city5'
+    ];
 
-        // Проверяем, действительно ли дата совпадает с тем, что мы создали
-        if ($date && $date->format('Y-m-d') === $dateString) {
-            // Получаем день недели
-            $dayOfWeek = $date->format('l'); // Формат 'l' возвращает полное имя дня недели
-            return "День недели для {$dateString} - {$dayOfWeek}.";
-        } else {
-            return "Неверная дата.";
-        }
+    // Проверяем, существует ли имя пользователя в массиве
+    if (array_key_exists($name, $users)) {
+        // Возвращаем город пользователя
+        return "Город пользователя {$name} - {$users[$name]}.";
     }
 
-    return "Неверный формат параметров. Используйте: /{year}/{month}/{day}, где год - 4 цифры, месяц - 01 до 12, день - 01 до 31.";
-})->where([
-    'year' => '\d{4}', 
-    'month' => '0[1-9]|1[0-2]', 
-    'day' => '0[1-9]|[12][0-9]|3[01]'
-]);
+    // Если пользователь не найден, возвращаем сообщение об ошибке
+    return "Пользователь с именем {$name} не найден.";
+});
